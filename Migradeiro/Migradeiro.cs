2a846f49629ac2a4c36d6466e949e67daf6c1587
necotheone <ntho89@gmail.com>
2016-06-30 14:20:00 +0200

    Lineas de conexión Telnet

--- Migradeiro/Migradeiro.cs
+++ Migradeiro/Migradeiro.cs
@@ -24,6 +24,7 @@ using System.Threading.Tasks;
 using System.Timers;
 using MinimalisticTelnet;
 using Migradeiro.Clases;
+using System.Threading;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -34,9 +35,9 @@ namespace Migradeiro
         // Variables globales
         private string logRoute = @"C:\Ejecutables\Migra2\LOGS\";
         private string tempRoute = @"C:\Ejecutables\Migra2\Temp\";
-        private string tempFile = "HLRTemp.txt";
         private string logFile = "MIGRALOG.txt";
         private Log log;
+        private TelnetConnection tc;
 
         // Inicia aquí la conexión con la BBDD. Para ello tiene que existir la línea de conexión correspondiente
         // en el fichero de tnsnames.ora
@@ -75,7 +76,7 @@ namespace Migradeiro
                 sw.Close();
             }
             log = new Log(logRoute, logFile);
-            Timer timer = new Timer();
+            System.Timers.Timer timer = new System.Timers.Timer();
             timer.Interval = 120000; // 120 segundos
             timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
             timer.Start();
@@ -87,15 +88,35 @@ namespace Migradeiro
         {
             // Función principal de servicio. Cada dos minutos, el timer lanzará un evento que se recoge en esta 
             // función. Se debe hacer un telnet al HLR y procesar el fichero resultante hacia la BBDD.
-            TelnetConnection tc;
             try
             {
                 tc = new TelnetConnection("10.2.144.75", 23);
+                log.WriteLine("Conexión realizada correctamente");
             }
             catch (Exception e)
             {
                 log.WriteLine("No se ha podido abrir la conexión TELNET", "ERROR");
                 log.WriteLine("Mensaje: " + e.Message.ToString(), "ERROR");
+            }
+            Thread.Sleep(600);
+            tc.Write("ingenieria\n\r");
+            Thread.Sleep(600);
+            tc.Write("ing2010AXE\n\r");
+            Thread.Sleep(600);
+            tc.Write("\n\r");
+            Thread.Sleep(600);
+            tc.WriteLine("mml\n\r");
+            Thread.Sleep(600);
+            using (StreamWriter sw = new StreamWriter(Path.Combine(tempRoute, DateTime.Now.ToShortDateString() + ".txt")))
+            {
+                sw.WriteLine("Fecha de creación del informe: " + DateTime.Now.ToLongDateString());
+                sw.WriteLine("Resultados de consulta de HLR");
+                sw.WriteLine();
+                sw.Write(tc.Read() + "\n\r");
+                log.WriteLine("Autenticado");
+                log.WriteLine("Petición de líneas en espera");
+                //tc.Write("HGICP:NIMSI=ALL;\n\r");                       // Línea de pruebas
+                tc.Write("HGICP:NIMSI=ALL,EXEC;\n\r");                //Línea de ejecución
             }
         }
 
@@ -105,6 +126,7 @@ namespace Migradeiro
         {
             using (StreamWriter sw = File.AppendText(Path.Combine(logRoute, logFile)))
             {
+                sw.WriteLine();
                 sw.WriteLine("                        FIN DE SERVICIO                       ");
                 sw.WriteLine("--------------------------------------------------------------");
                 sw.WriteLine();
