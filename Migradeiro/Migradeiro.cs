3fc40b528761c0b1431f5f2cbfa8f52b3a053049
necotheone <ntho89@gmail.com>
2016-06-30 13:17:44 +0200

    Agregado soporte Telnet

--- Migradeiro/Migradeiro.cs
+++ Migradeiro/Migradeiro.cs
@@ -22,6 +22,7 @@ using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
 using System.Timers;
+using MinimalisticTelnet;
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
